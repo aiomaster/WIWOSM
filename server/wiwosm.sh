4bdcc9bfadef4f6d95469460d7f84ca3e5f95a98
Christoph Wagner <wagner.naunhof@freenet.de>
2012-03-25 21:38:26 +0200

    qcronsub shell skript for updating wiwosm geojson

---
+++ server/wiwosm.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+#$ -N wiwosm
+#$ -l h_rt=2:00:00
+#$ -l virtual_free=20M
+#$ -l sql-toolserver=1
+#$ -l sql-mapnik=1
+#$ -m a
+#$ -o $HOME/log/wiwosm.out
+#$ -e $HOME/log/wiwosm.err
+
+
+### on wednesday do a full upgrade (takes 84min)
+### otherdays just update one file for every article (takes 35 min)
+if [ `date +%u` -eq 3 ]
+then
+	php /home/master/gen_json_files.php full
+else
+	php /home/master/gen_json_files.php
+fi
